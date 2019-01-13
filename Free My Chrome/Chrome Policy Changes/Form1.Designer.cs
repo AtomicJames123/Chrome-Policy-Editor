fcf6282d60308ad52c9138f2dfaa9882112b7fd5
James Goodman <Jamesultima@Hotmail.com>
2019-01-13 15:06:22 -0500

    Older Version of Chrome Policy Editor with About Page Functionality
    Has About Page Functionality which was removed in the newer version and placed directly within windows properties details

---
+++ Free My Chrome/Chrome Policy Changes/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Chrome_Policy_Changes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Enable Extension Install",
+            "Enable Sign In ",
+            "Enable Sync",
+            "Enable Incognito mode"});
+            this.checkedListBox1.Location = new System.Drawing.Point(85, 38);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(136, 75);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(204, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+//            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(241, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 36);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Enable Chrome Restrictions";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "About";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(156, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 2);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "By Rahul and James";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(-4, -1);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(349, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "Please choose the appropriate modifications:\r\n\r\n\r\n";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(344, 169);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Free My Chrome";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Free My Chrome/Chrome Policy Changes/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Chrome_Policy_Changes
+{
+    public partial class Form1 : Form
+    {
+        public bool extensions;
+        public bool signin;
+        public bool incognito;
+        public bool sync;
+
+        public Form1()
+        {
+            this.KeyPreview = true;
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (checkedListBox1.SelectedIndex == 0 && checkedListBox1.SelectedIndex > -1)
+            {
+                extensions = !extensions;
+              /*  if (extensions == true)
+                {
+                    MessageBox.Show("Checked");
+                }
+
+                if (extensions == false)
+                {
+                    MessageBox.Show("Unchecked");
+                }*/
+
+            }
+
+           /* if (checkedListBox1.SelectedIndex == 1 && checkedListBox1.SelectedIndex > -1)
+            {
+                search = !search;
+              /*  if (search == true)
+                {
+                    MessageBox.Show("Search");
+                }
+
+                if (search == false)
+                {
+                    MessageBox.Show("Unchecked");
+                }
+
+            }*/
+
+            if (checkedListBox1.SelectedIndex == 1 && checkedListBox1.SelectedIndex > -1)
+            {
+                signin = !signin;
+              /*  if (signin == true)
+                {
+                    MessageBox.Show("Sign In");
+                }
+
+                if (signin == false)
+                {
+                    MessageBox.Show("Unchecked");
+                }*/
+
+            }
+
+            if (checkedListBox1.SelectedIndex == 2 && checkedListBox1.SelectedIndex > -1)
+            {
+                sync = !sync;
+              /* if (bookmarks == true)
+                {
+                    MessageBox.Show("Bookmarks");
+                }
+
+                if (bookmarks == false)
+                {
+                    MessageBox.Show("Unchecked");
+                }*/
+
+            }
+
+            if (checkedListBox1.SelectedIndex == 3 && checkedListBox1.SelectedIndex > -1)
+            {
+                incognito = !incognito;
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(extensions == true)
+            {
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Google\\Chrome\\ExtensionInstallBlacklist", "1", "");
+                /*dialog = MessageBox.Show("Do you want to make any other changes?", "Changes have been made!", MessageBoxButtons.YesNo);
+                if (dialog == DialogResult.No)
+                {
+                    Application.Exit();
+                }*/
+            }
+
+            /*if(search == true)
+            {
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Google\\Chrome", "ForceGoogleSafeSearch", "0", RegistryValueKind.DWord);
+            }*/
+
+            if(signin == true)
+            {
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Google\\Chrome", "RestrictSigninToPattern", "");
+            }
+
+           if(incognito == true)
+            {
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Google\\Chrome", "IncognitoModeAvailability", "0", RegistryValueKind.DWord);
+            }
+
+           if(sync == true)
+            {
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Google\\Chrome", "SyncDisabled", "0", RegistryValueKind.DWord);
+            }
+
+            if (extensions == false && signin == false && incognito == false && sync == false){
+                MessageBox.Show("No selection has been made!", "Error!");
+            }
+
+            else
+            {
+                MessageBox.Show("Changes have been made! You will need to restart Google Chrome for changes to take effect!");
+                Application.Exit();
+            }
+           
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //extensions
+            Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Google\\Chrome\\ExtensionInstallBlacklist", "1", "*");
+            //search
+            //Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Google\\Chrome", "ForceGoogleSafeSearch", "1", RegistryValueKind.DWord);
+            //signin
+            Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Google\\Chrome", "RestrictSigninToPattern", "*@schools.nyc.gov");
+            //Incognito Mode
+            Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Google\\Chrome", "IncognitoModeAvailability", "1", RegistryValueKind.DWord);
+            //Sync
+            Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Google\\Chrome", "SyncDisabled", "1", RegistryValueKind.DWord);
+
+            MessageBox.Show("Settings have been restored to default!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AboutBox1 aboutBox1 = new AboutBox1();
+            aboutBox1.ShowDialog();
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Rahul & James");
+        }
+
+        private void UpdateGroupPolicy()
+        {
+            FileInfo execfile = new FileInfo("gpupdate.exe");
+            Process proc = new Process();
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.StartInfo.FileName = execfile.Name;
+            proc.StartInfo.Arguments = "/force";
+            proc.Start();
+
+            while (!proc.HasExited)
+            {
+                Application.DoEvents();
+                Thread.Sleep(100);
+            }
+        }
+
+    }
+}
